41f4f6de2a4d4fea7b992f9fcaa9ce9f97d5945a
mbugua <email@email.com>
2021-02-24 11:34:54 +0300

    final design for scrapper

--- Scrapper/urls.py
+++ Scrapper/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from collector import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.scrap, name='scrap'),
+    path('delete/', views.clear, name='clear'),
 ]
